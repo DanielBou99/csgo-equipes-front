c09aa584c8fbdd1a6d465011eac0295038d52b5b
Daniel <danielbou@outlook.com.br>
2022-07-05 10:26:02 -0300

    Componente Login e componente inicial Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './autenticar/cadastro/cadastro.component';
+import { LoginComponent } from './autenticar/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  { path: 'cadastro', component: CadastroComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
